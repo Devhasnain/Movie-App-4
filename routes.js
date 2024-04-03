24ed277b6f10fe5aacc5971678d5f10420fe3e32
unknown <hasnainalam1166@gmail.com>
2024-04-03 14:42:40 +0500

    app ready fix movie details page

---
+++ routes.js
@@ -0,0 +1,13 @@
+// import { API_KEY } from "@env";
+const API_KEY = "186fb70cd1f15675c40438d3d7ae0899";
+export const baseUrl = "https://api.themoviedb.org/3";
+export const imageUrl = "https://image.tmdb.org/t/p/original";
+
+export const route = {
+  now_playing: `${baseUrl}/movie/now_playing?api_key=${API_KEY}&language=en-US`,
+  popular: `${baseUrl}/movie/popular?api_key=${API_KEY}&language=en-US`,
+  top_rated: `${baseUrl}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  upcoming: `${baseUrl}/movie/upcoming?api_key=${API_KEY}&language=en-US`,
+  trending: `${baseUrl}/trending/movie/week?api_key=${API_KEY}&language=en-US`,
+  search: `${baseUrl}/search/movie?api_key=${API_KEY}&language=en-US`
+};
